fe70abe762ea064834fc2c6b4d32505d25b80d5e
Aurelien ALET <aalet@sii.fr>
2022-05-29 17:36:26 +0200

    fix: update iDisplayHeroes interface according to previous changes

--- src/app/domain/ports/i-display-heroes.ts
+++ src/app/domain/ports/i-display-heroes.ts
@@ -4,9 +4,10 @@ import { Hero } from "src/app/domain/models/hero"
 export default interface IDisplayHeroes {
 
     heroes: Hero[]
+    filter: string
     
     askHeroesList(): Observable<void>
-    askHeroesFiltered(filter: string): Observable<void>
+    askHeroesFiltered(filter: string, allowEmpty?: boolean): Observable<void>
     askHeroCreation(heroName: string): Observable<void>
     askHeroDeletion(hero: Hero): Observable<void>
 
