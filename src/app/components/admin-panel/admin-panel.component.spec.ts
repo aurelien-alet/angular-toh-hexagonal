d75cb4a46f78855a44e55a679c6a7b1cdddf6516
Aurelien ALET <aalet@sii.fr>
2022-05-29 21:06:37 +0200

    add: admin components tests

--- src/app/components/admin-panel/admin-panel.component.spec.ts
+++ src/app/components/admin-panel/admin-panel.component.spec.ts
@@ -1,80 +1,103 @@
-// import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-// import { RouterTestingModule } from "@angular/router/testing";
-// import { of } from 'rxjs';
-// import IDisplayHeroDetail from 'src/app/domain/ports/i-display-hero-detail';
-// import { Location } from '@angular/common';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { of } from 'rxjs';
 
-// import { AdminPanelComponent } from './admin-panel.component';
-// import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import { AdminPanelComponent } from './admin-panel.component';
+import IDisplayHeroes from 'src/app/domain/ports/i-display-heroes';
 
-// describe('AdminPanelComponent', () => {
-//   let component: AdminPanelComponent;
-//   let fixture: ComponentFixture<AdminPanelComponent>;
-//   let spyIDisplayHeroDetail: jasmine.SpyObj<IDisplayHeroDetail>;
-//   let spyLocation: jasmine.SpyObj<Location>;
+describe('AdminPanelComponent', () => {
+  let component: AdminPanelComponent;
+  let fixture: ComponentFixture<AdminPanelComponent>;
+  let spyIDisplayHeroes: jasmine.SpyObj<IDisplayHeroes>;
+  let displayedHeroes = [{id: 0, name: 'hero'}];
 
-//   beforeEach(async () => {
-//     spyIDisplayHeroDetail = jasmine.createSpyObj(
-//       'IDisplayHeroDetail', 
-//       ['askHeroDetail', 'askHeroNameChange'],
-//       {hero: {name: '', id: 0}}
-//     );
-//     spyIDisplayHeroDetail.askHeroDetail.and.returnValue(of());
-//     spyIDisplayHeroDetail.askHeroNameChange.and.returnValue(of());
+  beforeEach(async () => {
+    spyIDisplayHeroes = jasmine.createSpyObj(
+      'IDisplayHeroes', 
+      ['askHeroesList', 'askHeroCreation', 'askHeroDeletion', 'askHeroesFiltered'], 
+      {heroes: displayedHeroes}
+    );
+    spyIDisplayHeroes.askHeroesList.and.returnValue(of());
+    spyIDisplayHeroes.askHeroCreation.and.returnValue(of());
+    spyIDisplayHeroes.askHeroDeletion.and.returnValue(of());
+    spyIDisplayHeroes.askHeroesFiltered.and.returnValue(of());
 
-//     spyLocation = jasmine.createSpyObj(Location, ['back'])
+    await TestBed.configureTestingModule({
+      declarations: [ AdminPanelComponent ],
+      providers: [
+        { provide: 'IDisplayHeroes', useValue: spyIDisplayHeroes }
+      ]
+    })
+    .compileComponents();
+  });
 
-//     await TestBed.configureTestingModule({
-//       declarations: [ AdminPanelComponent ],
-//       providers: [
-//         { provide: 'IDisplayHeroDetail', useValue: spyIDisplayHeroDetail }, 
-//         { provide: Location, useValue: spyLocation },
-//         {
-//           provide: ActivatedRoute,
-//           useValue:  {snapshot: { paramMap: convertToParamMap({id: 0}) }}
-//         },
-//       ],
-//       imports: [
-//         RouterTestingModule.withRoutes([{path: 'detail/0', component: AdminPanelComponent}])
-//       ]
-//     })
-//     .compileComponents();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AdminPanelComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(AdminPanelComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
+  it('should ask heroes list', () => {
+    expect(spyIDisplayHeroes.askHeroesList).toHaveBeenCalledOnceWith();
+  });
 
-//   it('should ask hero details once', () =>{
-//     expect(spyIDisplayHeroDetail.askHeroDetail).toHaveBeenCalledOnceWith(0);
-//   });
+  it('should ask hero creation', waitForAsync(() => {
+    let input = fixture.debugElement.nativeElement.querySelector('#admin-new-hero');
+    input.value = 'new_hero';
+    input.dispatchEvent(new Event('input'));
 
-//   it('should navigate back on go back button click', waitForAsync(() => {
-//     let button = fixture.debugElement.nativeElement.querySelector('#go-back-button');
-//     button.click();
-
-//     fixture.whenStable().then(() => {
-//       expect(spyLocation.back).toHaveBeenCalledOnceWith();
-//     });
-//   }));
-
-//   it('should ask hero name change navigate back on hero name change button click', waitForAsync(() => {
-//     let input = fixture.debugElement.nativeElement.querySelector('#hero-name');
-//     input.value = 'new_name';
-//     input.dispatchEvent(new Event('input'));
-
-//     let button = fixture.debugElement.nativeElement.querySelector('#change-name-button');
-//     button.click();
+    let button = fixture.debugElement.nativeElement.querySelector('.add');
+    button.click();
     
-//     fixture.whenStable().then(() => {
-//       expect(spyIDisplayHeroDetail.askHeroNameChange).toHaveBeenCalledOnceWith('new_name');
-//       expect(spyLocation.back).toHaveBeenCalledOnceWith();
-//     });
-//   }));
-// });
+    fixture.whenStable().then(() => {
+      expect(spyIDisplayHeroes.askHeroCreation).toHaveBeenCalledOnceWith('new_hero');
+    });
+  }));
+
+  it('should ask hero deletion', waitForAsync(() => {
+    let delButton = fixture.debugElement.nativeElement.querySelector('.delete');
+    delButton.click();
+
+    fixture.whenStable().then(() => {
+      expect(spyIDisplayHeroes.askHeroDeletion).toHaveBeenCalledOnceWith({id: 0, name: 'hero'});
+    });
+}));
+
+it('should ask hero edition', waitForAsync(() => {
+    let editButton = fixture.debugElement.nativeElement.querySelector('.edit');
+    editButton.click();
+    
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(component.selectedHero).toEqual({id: 0, name: 'hero'});
+      expect(fixture.debugElement.nativeElement.querySelector('.edited-hero')).toBeTruthy();
+    });
+  }));
+
+  it('should reload heroes on leaveEdit event', waitForAsync(() => {
+    component.selectedHero = displayedHeroes[0];
+    fixture.detectChanges();
+    let editedHero = fixture.debugElement.nativeElement.querySelector('.edited-hero');
+    editedHero.dispatchEvent(new Event('leaveEdit'));
+
+    fixture.whenStable().then(() => {
+      expect(component.selectedHero).toEqual(null);
+      // called on component create, then called on event
+      expect(spyIDisplayHeroes.askHeroesList).toHaveBeenCalledTimes(2);
+    });
+
+  }));
+
+  it('should filter heroes', waitForAsync(() => {
+    let input = fixture.debugElement.nativeElement.querySelector('#filter-box');
+    input.value = 'search_string';
+    input.dispatchEvent(new Event('input'));
+    
+    fixture.whenStable().then(() => {
+      expect(spyIDisplayHeroes.askHeroesFiltered).toHaveBeenCalledOnceWith('search_string', true);
+    });
+  }));
+});
--- src/app/components/hero-edit/hero-edit.component.spec.ts
+++ src/app/components/hero-edit/hero-edit.component.spec.ts
@@ -1,25 +1,69 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { of } from 'rxjs';
+import IDisplayHeroDetail from 'src/app/domain/ports/i-display-hero-detail';
 
-// import { HeroEditComponent } from './hero-edit.component';
+import { HeroEditComponent } from './hero-edit.component';
 
-// describe('HeroEditComponent', () => {
-//   let component: HeroEditComponent;
-//   let fixture: ComponentFixture<HeroEditComponent>;
+describe('HeroEditComponent', () => {
+  let component: HeroEditComponent;
+  let fixture: ComponentFixture<HeroEditComponent>;
+  let spyIDisplayHeroDetail: jasmine.SpyObj<IDisplayHeroDetail>;
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [ HeroEditComponent ]
-//     })
-//     .compileComponents();
-//   });
+  beforeEach(async () => {
+    spyIDisplayHeroDetail = jasmine.createSpyObj(
+      'IDisplayHeroDetail', 
+      ['askHeroDetail', 'askHeroNameChange'],
+      {hero: {name: 'A', id: 1}}
+    );
+    spyIDisplayHeroDetail.askHeroDetail.and.returnValue(of());
+    spyIDisplayHeroDetail.askHeroNameChange.and.returnValue(of());
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(HeroEditComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+    await TestBed.configureTestingModule({
+      declarations: [ HeroEditComponent ],
+      providers: [
+        { provide: 'IDisplayHeroDetail', useValue: spyIDisplayHeroDetail }
+      ]
+    })
+    .compileComponents();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HeroEditComponent);
+    component = fixture.componentInstance;
+    component.heroId = 1;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should ask hero details once', () =>{
+    expect(spyIDisplayHeroDetail.askHeroDetail).toHaveBeenCalledOnceWith(1);
+  });
+
+  it('should trigger leaveEdit event on back button click', waitForAsync(() => {
+    spyOn(component.leaveEdit, 'emit');
+    let button = fixture.debugElement.nativeElement.querySelector('#admin-back');
+    button.click();
+
+    fixture.whenStable().then(() => {
+      expect(component.leaveEdit.emit).toHaveBeenCalledOnceWith();
+    });
+  }));
+
+  it('should ask hero name change and leaveEdit event on save button click', waitForAsync(() => {
+    spyOn(component.leaveEdit, 'emit');
+    let input = fixture.debugElement.nativeElement.querySelector('#edit-hero-name');
+    input.value = 'new_name';
+    input.dispatchEvent(new Event('input'));
+
+    let button = fixture.debugElement.nativeElement.querySelector('#admin-save');
+    button.click();
+    
+    fixture.whenStable().then(() => {
+      expect(spyIDisplayHeroDetail.askHeroNameChange).toHaveBeenCalledOnceWith('new_name');
+      expect(component.leaveEdit.emit).toHaveBeenCalledOnceWith();
+    });
+  }));
+});
